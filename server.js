1c15207691a68ea902ebc92d82a42fa551391e47
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-03 18:09:21 +0930

    updated SW

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/transaction_database", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
